6f94ffcc3a20210b95b6caf84c15f062dbb9aa49
ankits <ankuradhey@gmail.com>
2015-11-20 22:25:55 +0530

    Cartstep defaulted to service

--- application/modules/default/views/helpers/CartSteps.php
+++ application/modules/default/views/helpers/CartSteps.php
@@ -2,7 +2,7 @@
 
 class Zend_View_Helper_CartSteps extends Zend_View_Helper_Partial
 {
-	public function getSteps($currentStep = 1, $orderType)
+	public function getSteps($currentStep = 1, $orderType='service')
 	{
             return $this->partial('index/cartsteps.phtml',array('currentStep'=>$currentStep, 'orderType'=>$orderType));
 	}
