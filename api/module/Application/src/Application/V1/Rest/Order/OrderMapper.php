c260c45ab1f75848becff166857d83e162bfa6ee
ankits <ankuradhey@gmail.com>
2015-12-23 02:25:01 +0530

    service name and package name returned instead of unique id

--- api/module/Application/src/Application/V1/Rest/Order/OrderMapper.php
+++ api/module/Application/src/Application/V1/Rest/Order/OrderMapper.php
@@ -53,18 +53,50 @@ class OrderMapper {
 
     public function fetchAll() {
         $select = new Select('orders');
+        $select->order('order_id desc');
         $paginatorAdapter = new DbSelect($select, $this->adapter);
         $collection = new OrderCollection($paginatorAdapter);
         return $collection;
     }
 
     public function fetchOne($userId) {
-        $select = new Select('orders');
-        $select->where(' order_user_id = "'.$userId.'" ');
-        $paginatorAdapter = new DbSelect($select, $this->adapter);
+        //fetch packages
+        $packages = $this->fetchPackages();
+        $services = $this->fetchServices();
+
+        $sql = 'SELECT * FROM orders WHERE order_user_id = ? order by order_id desc';
+        $result = $this->adapter->query($sql, array($userId));
+        $result = $result->toArray();
+        $resultArr = array();
+        foreach($result as $resultKey=>$resultVal){
+            if($resultVal['order_type'] == 'service'){
+                $serviceIds = explode(',',$resultVal['order_service_type']);
+                if(!empty($serviceIds) && count($serviceIds)){
+                    $serviceIds = array_map(function($val) use ($services){
+                       return  $services[$val];
+                    }, $serviceIds);
+
+                    $result[$resultKey]['order_service_type'] = implode(",",$serviceIds);
+                }
+            }
+            elseif($resultVal['order_type'] == 'package'){
+                $packageIds = explode(',',$resultVal['order_service_type']);
+                if(!empty($packageIds) && count($packageIds)){
+                    $packageIds = array_map(function($val) use ($packages){
+                       return  $packages[$val];
+                    }, $packageIds);
+
+                    $result[$resultKey]['order_service_type'] = implode(",",$packageIds);
+                }
+            }
+        }
+//        $resultSet = new \Zend\Db\ResultSet\ResultSet();
+//        $resultSet->initialize($result);
+//        $paginatorAdapter = new DbSelect($select, $this->adapter);
+        $paginatorAdapter = new \Zend\Paginator\Adapter\ArrayAdapter($result);
+//        $paginatorAdapter = new \Zend\Paginator\Adapter\ArrayAdapter($result);
         $collection = new OrderCollection($paginatorAdapter);
         return $collection;
-        
     }
     
     public function fetchByOrder($orderId) {
@@ -88,6 +120,28 @@ class OrderMapper {
             return false;
         }
         return true;
+    }
+    
+    public function fetchServices(){
+        $sql = 'SELECT * FROM services_master WHERE service_status = ?';
+        $resultset = $this->adapter->query($sql, array('1'));
+        $data = $resultset->toArray();
+        $servicesArr = array();
+        foreach($data as $val){
+            $servicesArr[$val['service_id']] = $val['service_name'];
+        }
+        return $servicesArr;
+    }
+    
+    public function fetchPackages(){
+        $sql = 'SELECT * FROM packages where ?';
+        $resultset = $this->adapter->query($sql, array('1'));
+        $data = $resultset->toArray();
+        $packagesArr = array();
+        foreach($data as $val){
+            $packagesArr[$val['package_id']] = $val['package_name'];
+        }
+        return $packagesArr;
     }
 
 }
