1fc7606c270779254479873f5ec60aa24853242f
ankits <ankuradhey@gmail.com>
2015-12-25 17:36:01 +0530

    order update with status and service tax and delivery charge

--- api/module/Deliveryboy/src/Deliveryboy/V1/Rest/Orderproducts/OrderproductsResource.php
+++ api/module/Deliveryboy/src/Deliveryboy/V1/Rest/Orderproducts/OrderproductsResource.php
@@ -1,47 +1,115 @@
 <?php
+
 namespace Deliveryboy\V1\Rest\Orderproducts;
 
 use ZF\ApiProblem\ApiProblem;
 use ZF\Rest\AbstractResourceListener;
 
-class OrderproductsResource extends AbstractResourceListener
-{
+class OrderproductsResource extends AbstractResourceListener {
+
     protected $mapper;
-    
-    public function __construct($mapper){
+
+    public function __construct($mapper) {
         $this->mapper = $mapper;
     }
+
     /**
      * Create a resource
      *
      * @param  mixed $data
      * @return ApiProblem|mixed
      */
-    public function create($data)
-    {
+    public function create($data) {
         //find order id is valid or not
-        $data = (array)$data;
+        $data = (array) $data;
 //        $res = $this->mapper->checkOrder($data['order_id']);
-        
 //        if(!$res)
 //            return new ApiProblem(405, 'Order id not found');
         $orderId = false;
-        foreach($data as $val){
-            $orderId = $val['order_id'];
-            //check for valid orders
-            $res = $this->mapper->checkOrder($val['order_id']);
-            
-            if(!$res)
-                return new ApiProblem(405, 'Order id not found');
-            
-            $this->mapper->insert($val);
+        $flagPackage = false;
+        $serviceList = null;
+        $orderStatus = 'picked';
+        $totalAmount = 0;
+        if (isset($data) && count($data)) {
+            $orderId = $data[0]['order_id'];
+            //get services list
+            $orderDetails = $this->mapper->getOrderMapper()->fetchByOrder($val['order_id']);
+            if ($orderDetails['order_type'] == 'service') {
+                $serviceList = $orderDetails['order_service_type'];
+                $serviceList = explode(',', $serviceList);
+                //if no service found - order is not proper
+                if (count($serviceList) == 0 || empty($serviceList)) {
+                    return new ApiProblem(405, 'Incorrect order. No service selected.');
+                }
+            }
+
+            foreach ($data as $val) {
+                //check for valid orders
+                $res = $this->mapper->checkOrder($val['order_id']);
+
+                if (!$res)
+                    return new ApiProblem(405, 'Order id not found');
+
+                //===check if new service is there then add in order table
+                //--- 1. get services list (to check from the currently adding product + services list)
+                $serviceList;
+                //--- 2. Check if service is new - if found then update order_products table
+                if (!in_array($val['order_service'], $serviceList) && isset($val['order_service_name'])) {
+                    $orderUpdateArr = array();
+                    $serviceList[] = $val['order_service_name'];
+                    $orderUpdateArr['order_service_type'] = implode(",", $serviceList);
+                    $orderUpdateArr['order_status'] = $val['order_status'];
+                    $ret = $this->orderMapper->update($orderUpdateArr, $orderId);
+                }
+
+                if (!isset($val['order_product_id']))
+                    $this->mapper->insert($val);
+                else {
+                    $orderProductId = $val['order_product_id'];
+                    unset($val['order_product_id']);
+                    $this->mapper->update($val, $orderProductId);
+                }
+
+                if ($val['order_type'] == 'package') {
+                    $flagPackage = true;
+                }
+
+                if ($val['order_status'] == 'returned') {
+                    $orderStatus = 'returned';
+                } elseif ($val['order_status'] != 'delivered' && $orderStatus != 'returned') {
+                    $orderStatus = null;
+                } elseif (isset($val['order_status']) && isset($orderStatus) && $orderStatus != 'returned') {
+                    if ($val['order_status'] == 'returned') {
+                        $orderStatus = 'returned';
+                    } else {
+                        $orderStatus = $val['order_status'];
+                    }
+                }
+                $totalAmount += $val['total_price'];
+            }
+        } else {
+            return new ApiProblem(405, 'Order products not found');
         }
-        
-        if(!$orderId)
+
+        if (!$orderId)
             return new ApiProblem(405, 'Order id not found');
-        
-        $this->mapper->updatePackage($orderId, null, 1);
-        $this->mapper->updateOrderStatus($orderId);
+
+        if ($flagPackage)
+            $this->mapper->updatePackage($orderId, null, 1);
+
+        $updateOrderData = array();
+        $updateOrderData['order_status'] = $orderStatus;
+        if ($orderStatus == 'picked') {
+            $serviceTax = $totalAmount * 14 / 100;
+            $totalAmount += $totalAmount * 14 / 100;
+            $updateOrderData['service_tax'] = $totalAmount;
+            if($totalAmount < 200){
+                $totalAmount += 50;
+                $updateOrderData['delivery_charge'] = 50;
+                $updateOrderData['order_amount'] = $totalAmount;
+            }
+        }
+        $this->mapper->updateOrder($orderId, $updateOrderData);
         return true;
     }
 
@@ -51,8 +119,7 @@ class OrderproductsResource extends AbstractResourceListener
      * @param  mixed $id
      * @return ApiProblem|mixed
      */
-    public function delete($id)
-    {
+    public function delete($id) {
         return new ApiProblem(405, 'The DELETE method has not been defined for individual resources');
     }
 
@@ -62,8 +129,7 @@ class OrderproductsResource extends AbstractResourceListener
      * @param  mixed $data
      * @return ApiProblem|mixed
      */
-    public function deleteList($data)
-    {
+    public function deleteList($data) {
         return new ApiProblem(405, 'The DELETE method has not been defined for collections');
     }
 
@@ -73,8 +139,7 @@ class OrderproductsResource extends AbstractResourceListener
      * @param  mixed $id
      * @return ApiProblem|mixed
      */
-    public function fetch($id)
-    {
+    public function fetch($id) {
         return $this->mapper->fetchOrderProducts($id);
     }
 
@@ -84,8 +149,7 @@ class OrderproductsResource extends AbstractResourceListener
      * @param  array $params
      * @return ApiProblem|mixed
      */
-    public function fetchAll($params = array())
-    {
+    public function fetchAll($params = array()) {
         return new ApiProblem(405, 'The GET method has not been defined for collections');
     }
 
@@ -96,8 +160,7 @@ class OrderproductsResource extends AbstractResourceListener
      * @param  mixed $data
      * @return ApiProblem|mixed
      */
-    public function patch($id, $data)
-    {
+    public function patch($id, $data) {
         return new ApiProblem(405, 'The PATCH method has not been defined for individual resources');
     }
 
@@ -107,8 +170,7 @@ class OrderproductsResource extends AbstractResourceListener
      * @param  mixed $data
      * @return ApiProblem|mixed
      */
-    public function replaceList($data)
-    {
+    public function replaceList($data) {
         return new ApiProblem(405, 'The PUT method has not been defined for collections');
     }
 
@@ -119,8 +181,8 @@ class OrderproductsResource extends AbstractResourceListener
      * @param  mixed $data
      * @return ApiProblem|mixed
      */
-    public function update($id, $data)
-    {
+    public function update($id, $data) {
         return new ApiProblem(405, 'The PUT method has not been defined for individual resources');
     }
+
 }
