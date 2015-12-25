4aaa5cbd391986c3c6cd125561faf1bece574383
ankits <ankuradhey@gmail.com>
2015-12-25 17:34:23 +0530

    updated delivery date and status on some conditions

--- api/module/Deliveryboy/src/Deliveryboy/Mapper/DeliveryboyMapper.php
+++ api/module/Deliveryboy/src/Deliveryboy/Mapper/DeliveryboyMapper.php
@@ -10,31 +10,49 @@ use Zend\Db\Adapter;
 use Deliveryboy\Entity\DeliveryboyEntity as DeliveryboyEntity;
 use Application\V1\Rest\Order\OrderEntity as OrderEntity;
 use Application\V1\Rest\Order\OrderCollection as OrderCollection;
+use Zend\ServiceManager\ServiceManager as ServiceManager;
+
 class DeliveryboyMapper {
 
-    protected $adapter;
+    protected $adapter, $serviceManger, $orderMapper;
 
-    public function __construct(AdapterInterface $adapter) {
+    public function __construct(AdapterInterface $adapter, ServiceManager $sm) {
         $this->adapter = $adapter;
+        $this->serviceManger = $sm;
     }
-    
-    public function updateOrderStatus($orderId, $orderStatus = 'pickup'){
-        $sql = "update orders set order_status = '$orderStatus' WHERE order_id = ? ";
-        $resultset = $this->adapter->query($sql, array($orderId));
-        return $resultSet;
+
+    public function getOrderMapper() {
+        if (!isset($orderMapper)) {
+            $this->orderMapper = $this->serviceManager->get('Application\V1\Rest\Order\OrderMapper');
+        }
+        return $this->orderMapper;
     }
+
+    public function updateOrder($orderId, $data) {
+        if (isset($data['order_status'])) {
+            if ($data['order_status'] == 'returned') {
+                $sql = "update orders set order_delivery = DATE_ADD(order_delivery, interval 2 day) WHERE order_id = ? ";
+            } else {
+                $sql = "update orders set order_status = '".$data['order_status']."' WHERE order_id = ? ";
+            }
             
-    
-    public function updatePackage($orderId, $clothesLeft = null, $pickupsLeft = null){
+            $resultset = $this->adapter->query($sql, array($orderId));
+            return $resultset;
+        } else {
+            return false;
+        }
+    }
+
+    public function updatePackage($orderId, $clothesLeft = null, $pickupsLeft = null) {
         $setSql = ' set ';
-        if($clothesLeft){
-            $setSql = ' clothes_left = clothes_left - '.(int)$clothesLeft;
+        if ($clothesLeft) {
+            $setSql = ' clothes_left = clothes_left - ' . (int) $clothesLeft;
         }
-        
-        if($pickupsLeft){
-            $setSql = ' pickups_left = pickups_left - '.(int)$pickupsLeft;
+
+        if ($pickupsLeft) {
+            $setSql = ' pickups_left = pickups_left - ' . (int) $pickupsLeft;
         }
-        
+
         $sql = "update user_track ut
                     $set
                     join order o on o.order_service_type = ut.usertrack_package_id and  o.order_user_id = ut.usertrack_user_id and order_delivery_status is NULL and usertrack_expiry_date >= now()  
@@ -42,22 +60,43 @@ class DeliveryboyMapper {
                     limit 1 ";
         $resultset = $this->adapter->query($sql, array($orderId));
     }
-    
-    public function insert($data){
+
+    public function insert($data) {
         $adapter = $this->adapter;
         $sql = new Sql($this->adapter);
         $insert = $sql->insert('order_products');
         $insert->values($data);
         $selectString = $sql->getSqlStringForSqlObject($insert);
         $results = $this->adapter->query($selectString, $adapter::QUERY_MODE_EXECUTE);
-        
+
         //if type package is there then reduce number of clothes and days
-        if($results && $data['order_type'] == 'package'){
-            $this->updatePackage($data['order_id'],$data['quantity']);
+        if ($results && $data['order_type'] == 'package') {
+            $this->updatePackage($data['order_id'], $data['quantity']);
         }
     }
-    
-    public function authenticate($userName, $password){
+
+    //updating order_product relation
+    public function update($data, $orderProductId) {
+        $adapter = $this->adapter;
+        $sql = new Sql($adapter);
+        $update = $sql->update();
+        $update->table('order_products');
+        $update->set($data);
+        $update->where(array('order_product_id' => $orderProductId));
+        $statement = $sql->prepareStatementForSqlObject($update);
+        try {
+            $affectedRows = $statement->execute()->getAffectedRows();
+        } catch (\Exception $e) {
+            die('Error: ' . $e->getMessage());
+        }
+        if (empty($affectedRows)) {
+            die('Zero rows affected');
+        }
+
+        return $affectedRows;
+    }
+
+    public function authenticate($userName, $password) {
         $sql = 'SELECT * FROM delivery_boy WHERE delboy_username = ? and delboy_password = ? ';
         $resultset = $this->adapter->query($sql, array($userName, $password));
         $data = $resultset->toArray();
@@ -69,27 +108,30 @@ class DeliveryboyMapper {
         $entity->exchangeArray($data[0]);
         return $entity;
     }
-    
-    public function fetchOrders($id, $orderStatus = NULL){
+
+    public function fetchOrders($id, $orderStatus = NULL) {
         $select = new Select('orders');
-        $select->where(" order_delivery_boy = '$id' ");
+
         $select->join(
-                    array('ut'=>'user_track'), ' ut.usertrack_package_id = orders.order_service_type ',array('clothes_left','pickups_left'),'left'
-                );
-        
-        
-        if($orderStatus){
-            if($orderStatus == 'pickup')
-                $select->where(" order_status is NULL  ");
-            elseif($orderStatus == 'delivery')
-                $select->where(" order_status = 'picked'  ");
+                array('ut' => 'user_track'), ' ut.usertrack_package_id = orders.order_service_type ', array('clothes_left', 'pickups_left'), 'left'
+        );
+
+
+        if ($orderStatus) {
+            if ($orderStatus == 'pickup') {
+                $select->where(" order_pickup_boy = '$id' and order_status is NULL ");
+//                $select->where(" order_status is NULL  ");
+            } elseif ($orderStatus == 'delivery') {
+                $select->where(" order_delivery_boy = '$id' and order_status is 'picked' ");
+//                $select->where(" order_status = 'picked'  ");
+            }
         }
         $paginatorAdapter = new DbSelect($select, $this->adapter);
         $collection = new OrderCollection($paginatorAdapter);
         return $collection;
     }
-    
-    public function checkOrder($orderId){
+
+    public function checkOrder($orderId) {
         $sql = 'SELECT * FROM orders WHERE order_id = ? ';
         $resultset = $this->adapter->query($sql, array($orderId));
         $data = $resultset->toArray();
@@ -101,8 +143,8 @@ class DeliveryboyMapper {
         $entity->exchangeArray($data[0]);
         return $entity;
     }
-    
-    public function checkDeliveryBoy($id){
+
+    public function checkDeliveryBoy($id) {
         $sql = 'SELECT * FROM delivery_boy WHERE delboy_id = ? ';
         $resultset = $this->adapter->query($sql, array($id));
         $data = $resultset->toArray();
@@ -114,12 +156,13 @@ class DeliveryboyMapper {
         $entity->exchangeArray($data[0]);
         return $entity;
     }
-    
-    public function fetchOrderProducts($orderId){
+
+    public function fetchOrderProducts($orderId) {
         $select = new Select('order_products');
         $select->where(" order_id = '$orderId' ");
         $paginatorAdapter = new DbSelect($select, $this->adapter);
         $collection = new OrderCollection($paginatorAdapter);
         return $collection;
     }
+
 }
