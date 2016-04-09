b6b59918b6b9e155119aa72842731bacdacf7668
ankits <ankuradhey@gmail.com>
2016-04-09 22:29:24 +0530

    user transaction code for api

--- api/module/Application/src/Application/V1/Rest/Order/OrderResource.php
+++ api/module/Application/src/Application/V1/Rest/Order/OrderResource.php
@@ -1,46 +1,74 @@
 <?php
+
 namespace Application\V1\Rest\Order;
 
 use ZF\ApiProblem\ApiProblem;
 use ZF\Rest\AbstractResourceListener;
 
-class OrderResource extends AbstractResourceListener
-{
+class OrderResource extends AbstractResourceListener {
+
     public $mapper, $userMapper, $services;
-    
+
     public function __construct($services) {
         $this->services = $services;
         $this->mapper = $services->get('Application\V1\Rest\Order\OrderMapper');
     }
-    
-    protected function getUserMapper(){
-        if(!isset($this->userMapper)){
+
+    protected function getUserMapper() {
+        if (!isset($this->userMapper)) {
             $this->userMapper = $this->services->get('Application\V1\Rest\User\UserMapper');
         }
         return $this->userMapper;
     }
+
     /**
      * Create a resource
      *
      * @param  mixed $data
      * @return ApiProblem|mixed
      */
-    public function create($data)
-    {
-        //check if valid user
-        $data = (array)$data;
-        //returns user info
-        $res = $this->mapper->checkUser((int)$data['order_user_id']);
-        
-        if(!$res)
-            return new ApiProblem(405, 'User with id '.$data['order_user_id'].' not found');
-        
-        $ret = $this->mapper->insert($data);
-        if(!isset($res[0]['user_email'])){
-            $userMapper = $this->getUserMapper();
-            $userMapper->update(array('user_email'=>$data['order_user_email']),$data['order_user_id']);
+    public function create($data) {
+        try {
+            //check if valid user
+            $data = (array) $data;
+            //returns user info
+            $res = $this->mapper->checkUser((int) $data['order_user_id']);
+
+            if (!$res)
+                return new ApiProblem(405, 'User with id ' . $data['order_user_id'] . ' not found');
+
+            $ret = $this->mapper->insert($data);
+            
+            //exception handling - if order not saved
+            if(empty($ret['order_id'])){
+                return new ApiProblem(405, $e->getMessage());
+            }
+            
+            //if transaction_id found then - order is already paid
+            if($ret['transaction_id']){
+                $trxnArr = array();
+                $trxnArr['order_id'] = $ret['order_id'];
+                $trxnArr['user_id'] = $ret['order_user_id'];
+                $trxnArr['gateway_transaction_id'] = $ret['transaction_id'];
+                $trxnArr['trnx_amount'] = $ret['order_amount'];
+                //TO DO
+                $trxnArr['trnx_method'] = 'creditcard';
+                $trxnArr['trnx_status'] = 'success';
+                $trxn = $this->mapper->createTransaction($trxnArr);
+                
+                if(empty($trxn['trnx_id'])){
+                    return new ApiProblem(405, 'Oops! Some transaction error occurred');
+                }
+            }
+            
+            if (!isset($res[0]['user_email'])) {
+                $userMapper = $this->getUserMapper();
+                $userMapper->update(array('user_email' => $data['order_user_email']), $data['order_user_id']);
+            }
+            return $ret;
+        } catch (Exception $e) {
+            return new ApiProblem(405, $e->getMessage());
         }
-        return $ret;
     }
 
     /**
@@ -49,8 +77,7 @@ class OrderResource extends AbstractResourceListener
      * @param  mixed $id
      * @return ApiProblem|mixed
      */
-    public function delete($id)
-    {
+    public function delete($id) {
         return new ApiProblem(405, 'The DELETE method has not been defined for individual resources');
     }
 
@@ -60,8 +87,7 @@ class OrderResource extends AbstractResourceListener
      * @param  mixed $data
      * @return ApiProblem|mixed
      */
-    public function deleteList($data)
-    {
+    public function deleteList($data) {
         return new ApiProblem(405, 'The DELETE method has not been defined for collections');
     }
 
@@ -71,8 +97,7 @@ class OrderResource extends AbstractResourceListener
      * @param  mixed $id
      * @return ApiProblem|mixed
      */
-    public function fetch($id)
-    {
+    public function fetch($id) {
         return $this->mapper->fetchByOrder($id);
         //return new ApiProblem(405, 'The GET method has not been defined for individual resources');
     }
@@ -83,11 +108,9 @@ class OrderResource extends AbstractResourceListener
      * @param  array $params
      * @return ApiProblem|mixed
      */
-    public function fetchAll($params = array())
-    {
-        $userId =  $params->get('user_id');
+    public function fetchAll($params = array()) {
+        $userId = $params->get('user_id');
         return $this->mapper->fetchOne($userId);
-        
     }
 
     /**
@@ -97,8 +120,7 @@ class OrderResource extends AbstractResourceListener
      * @param  mixed $data
      * @return ApiProblem|mixed
      */
-    public function patch($id, $data)
-    {
+    public function patch($id, $data) {
         return new ApiProblem(405, 'The PATCH method has not been defined for individual resources');
     }
 
@@ -108,8 +130,7 @@ class OrderResource extends AbstractResourceListener
      * @param  mixed $data
      * @return ApiProblem|mixed
      */
-    public function replaceList($data)
-    {
+    public function replaceList($data) {
         return new ApiProblem(405, 'The PUT method has not been defined for collections');
     }
 
@@ -120,8 +141,8 @@ class OrderResource extends AbstractResourceListener
      * @param  mixed $data
      * @return ApiProblem|mixed
      */
-    public function update($id, $data)
-    {
-        return $this->mapper->update((array)$data, (int)$id);
+    public function update($id, $data) {
+        return $this->mapper->update((array) $data, (int) $id);
     }
+
 }
